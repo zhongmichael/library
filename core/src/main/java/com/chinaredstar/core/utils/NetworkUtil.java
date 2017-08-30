8ccc726831b2aefa7d4f6db1ccb3215c1fe76644
{向海瑞} <hairui.xiang@chinaredstar.com>
2017-08-30 11:30:42 +0800

    1.0

--- core/src/main/java/com/chinaredstar/core/utils/NetworkUtil.java
+++ core/src/main/java/com/chinaredstar/core/utils/NetworkUtil.java
@@ -6,6 +6,11 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.telephony.TelephonyManager;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 /**
  * Created by hairui.xiang on 2017/8/29.
  */
@@ -140,8 +145,40 @@ public class NetworkUtil {
      */
     public static boolean isNetworkAvailable(Context context) {
         NetworkInfo info = getNetworkInfo(context);
-        if (null != info) {
-            return info.isAvailable();
+        return null != info && info.isAvailable();
+    }
+
+    /**
+     * 判断是否有外网连接（普通方法不能判断外网的网络是否连接，比如连接上局域网）
+     */
+    public static final boolean ping() {
+        String result = null;
+        try {
+            String ip = "www.baidu.com";// ping 的地址，可以换成任何一种可靠的外网
+            Process p = Runtime.getRuntime().exec("ping -c 3 -w 100 " + ip);// ping网址3次
+            // 读取ping的内容，可以不加
+            InputStream input = p.getInputStream();
+            BufferedReader in = new BufferedReader(new InputStreamReader(input));
+            StringBuffer stringBuffer = new StringBuffer();
+            String content = "";
+            while ((content = in.readLine()) != null) {
+                stringBuffer.append(content);
+            }
+            LogUtil.d("------ping-----", "result content : " + stringBuffer.toString());
+            // ping的状态
+            int status = p.waitFor();
+            if (status == 0) {
+                result = "success";
+                return true;
+            } else {
+                result = "failed";
+            }
+        } catch (IOException e) {
+            result = "IOException";
+        } catch (InterruptedException e) {
+            result = "InterruptedException";
+        } finally {
+            LogUtil.d("----result---", "result = " + result);
         }
         return false;
     }
