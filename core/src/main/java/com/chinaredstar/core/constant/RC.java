1ef375543dedd007a91d56ddc7ee1dba0a5dda20
{向海瑞} <hairui.xiang@chinaredstar.com>
2017-09-12 16:23:49 +0800

    1.0

--- core/src/main/java/com/chinaredstar/core/constant/RC.java
+++ core/src/main/java/com/chinaredstar/core/constant/RC.java
@@ -9,5 +9,5 @@ public class RC {// requestcode
     public static final int RC_FOR_PERMISSIONS = 4001;//动态权限申请
     public final static int RC_ACTION_IMAGE_CAPTURE = 4002;//打开相机
     public final static int RC_ACTION_PICK = 4003;//打开相册
-    public static final int RC_GET_QRCODE = 4003;//获取二维码
+    public static final int RC_GET_QRCODE = 4004;//获取二维码
 }
