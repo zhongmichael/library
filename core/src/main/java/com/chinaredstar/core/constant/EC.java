c49351e4bfcc0a85e5420458dc16913547bb8683
{向海瑞} <hairui.xiang@chinaredstar.com>
2017-09-14 17:45:02 +0800

    1.0

--- core/src/main/java/com/chinaredstar/core/constant/EC.java
+++ core/src/main/java/com/chinaredstar/core/constant/EC.java
@@ -6,4 +6,7 @@ package com.chinaredstar.core.constant;
 
 public class EC {//evnet code
     public static final int EC_DECODE_QR_CODE = 3000;//识别相册二维码
+    public static final int EC_CALCULATE_CACHE_SIZE = 3001;//计算缓存大小
+    public static final int EC_CLEAR_CACHE = 3002;//清除缓存
+    public static final int EC_COMPRESS_IMAGE = 3003;//压缩图片
 }
